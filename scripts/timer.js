3db034f46d22863e6b9aa34080b1dba6992cf958
grimmscore <73304217+grimmscore@users.noreply.github.com>
2023-06-02 06:25:41 +0300

    Update timer.js

--- scripts/timer.js
+++ scripts/timer.js
@@ -2,7 +2,7 @@
         let countdown_divs = document.getElementsByClassName("countdown");
 
         for(let i = 0; event_date_divs.length > i; i++){
-            event_date_divs[i].innerHTML = new Date(Date.parse(event_date_divs[i].getAttribute("data-start-event-date"))).toLocaleString("ru-RU", {
+            event_date_divs[i].innerHTML = new Date(Date.parse(event_date_divs[i].getAttribute("data-start-event-date"))).toLocaleString(window.navigator.language, {
                 month: "long", day: "numeric", hour: "2-digit", minute: "2-digit"
             });
         }
@@ -20,38 +20,73 @@
                     return;
                 }
 
+                let language = window.navigator.language;
                 let buf = "";
                 let days = Math.floor((event_date - current_time) / 86400000);
                 let hours = Math.floor(((event_date - current_time) / 3600000) % 24);
                 let minutes = Math.floor(((event_date - current_time) / 60000) % 60);
                 let seconds = Math.floor(((event_date - current_time) / 1000) % 60);
 
-                if (days > 0) {
-                    buf = days + " д. ";
-                }
 
-                if (hours > 0 || buf !== "") {
-                    if (buf !== "") {
-                        buf += "" + String(hours) + " ч. ";
-                    } else {
-                        buf = String(hours) + " ч. ";
+                switch(language) {
+                    case 'ru':
+                        if (days > 0) {
+                            buf = days + " д. ";
+                        }
+        
+                        if (hours > 0 || buf !== "") {
+                            if (buf !== "") {
+                                buf += "" + String(hours) + " ч. ";
+                            } else {
+                                buf = String(hours) + " ч. ";
+                            }
+                        }
+        
+                        if (minutes > 0 || buf !== "") {
+                            if (buf !== "") {
+                                buf += "" + String(minutes) + " м. ";
+                            } else {
+                                buf = String(minutes) + " м. ";
+                            }
+                        }
+        
+                        if (seconds > 0 || buf !== "") {
+                            if (buf !== "") {
+                                buf += "" + String(seconds) + " с.";
+                            } else {
+                                buf = String(seconds) + " с.";
+                            }
+                        }
+                        break
+                    default :
+                    if (days > 0) {
+                        buf = days + " d. ";
                     }
-                }
-
-                if (minutes > 0 || buf !== "") {
-                    if (buf !== "") {
-                        buf += "" + String(minutes) + " м. ";
-                    } else {
-                        buf = String(minutes) + " м. ";
+    
+                    if (hours > 0 || buf !== "") {
+                        if (buf !== "") {
+                            buf += "" + String(hours) + " h. ";
+                        } else {
+                            buf = String(hours) + " h. ";
+                        }
                     }
-                }
-
-                if (seconds > 0 || buf !== "") {
-                    if (buf !== "") {
-                        buf += "" + String(seconds) + " с.";
-                    } else {
-                        buf = String(seconds) + " с.";
+    
+                    if (minutes > 0 || buf !== "") {
+                        if (buf !== "") {
+                            buf += "" + String(minutes) + " m. ";
+                        } else {
+                            buf = String(minutes) + " m. ";
+                        }
                     }
+    
+                    if (seconds > 0 || buf !== "") {
+                        if (buf !== "") {
+                            buf += "" + String(seconds) + " s.";
+                        } else {
+                            buf = String(seconds) + " s.";
+                        }
+                    }
+                    break
                 }
 
                 countdown_divs[i].innerHTML = buf;
