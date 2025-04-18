474475ce9fff6869f8dae40ec8756c1b61a09998
Jinshoya <73304217+Jinshoya@users.noreply.github.com>
2025-04-18 22:18:02 +0300

    n

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    outDir: 'dist', // the directory to build the files into
-    assetsDir: 'assets',
     rollupOptions: {
-      input: 'index.html', // Make sure the entry point is correct
+      input: 'index.html',
     },
   },
 });
