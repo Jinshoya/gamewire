c3e8f77c40104a35b7fbcffae4e156310e99e669
Jinshoya <73304217+Jinshoya@users.noreply.github.com>
2025-04-18 21:47:23 +0300

    up

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-
+if (window.location.pathname.includes("trailers")) {
+  import('./trailers.js');
+}
 const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY
